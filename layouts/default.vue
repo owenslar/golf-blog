f0f29655e86ec5729b3c559f7864b2c290b65273
Owen <owenslar@byu.edu>
2025-03-18 12:15:59 -0600

    pages created with links between them

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <header>
+            <nav>
+                <NuxtLink to="/">Golf Blog</NuxtLink>
+                <ul>
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li><NuxtLink to="/about">About</NuxtLink></li>
+                    <li><NuxtLink to="/blogs">Blogs</NuxtLink></li>
+                </ul>
+            </nav>
+        </header>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
---
+++ pages/blogs/index.vue
@@ -0,0 +1,3 @@
+<template>
+    Blogs Page
+</template>
