356b8f2756aa6ead37fa158e5434c51d56dc8fa1
owenslar <owenslar@byu.edu>
2025-03-20 10:07:54 -0600

    blog card styling and display

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -1,14 +1,24 @@
 <template>
-    <div class="card text-center">
-        <p class="font-bold text-gray-500 m-4 truncate">{{ props.blog.username }} played a round!</p>
-        <p class="font-bold text-gray-500 m-4 truncate">They played {{ props.blog.scores.length }} holes and scored {{ score }}.</p>
-        <NuxtLink :to="`/blogs/${blog.id}`">
-            <p class="btn my-4 font-bold text-gray-700">View Details</p>
-        </NuxtLink>
+    <div class="card card-compact shadow-xl col-span-1 max-w-sm m-2 p-6 bg-white border gorder-gray-200 rounded-lg shadow-sm">
+        <div class="card-body">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">{{ props.blog.username }} played a round!</h5>
+            <h6 class="mb-2 text-2xl tracking-tight text-gray-700">{{ props.blog.course }}</h6>
+            <p class="mb-3 font-normal text-gray-700">They played {{ props.blog.scores.length }} holes and scored {{ score }}.</p>
+            <NuxtLink :to="`/blogs/${blog.id}`" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus-ring-4 focus:outline-none focus:ring-blue-300">
+                <p>View Details</p>
+            </NuxtLink>
+        </div>
     </div>
 </template>
 
 <script setup>
+
+// div class="card text-center"
+{/* <p class="font-bold text-gray-500 m-4 truncate">{{ props.blog.username }} played a round!</p>
+        <p class="font-bold text-gray-500 m-4 truncate">They played {{ props.blog.scores.length }} holes and scored {{ score }}.</p>
+        <NuxtLink :to="`/blogs/${blog.id}`">
+            <p class="btn my-4 font-bold text-gray-700">View Details</p>
+        </NuxtLink> */}
     const props = defineProps({
         blog: {
             type: Object,
--- pages/blogs/index.vue
+++ pages/blogs/index.vue
@@ -1,7 +1,9 @@
 <template>
-    Blogs Page
-    <div v-for="blog in blogs">
-        <BlogCard :blog="blog" />
+    <h1 class="mt-3 mb-4 text-2xl font-bold text-center">Check out what others are doing below!</h1>
+    <div class="flex flex-row flex-wrap">
+        <div v-for="blog in blogs">
+            <BlogCard :blog="blog" />
+        </div>
     </div>
 </template>
 
@@ -17,6 +19,27 @@
     {
         id: 2,
         username: 'owenlarson2',
+        course: 'Finkbine Golf Course',
+        scores: [2, 4, 3, 5, 4, 3, 6, 5, 4],
+        description: 'Played 9 at fink',
+    },
+    {
+        id: 2,
+        username: 'owenlarson3',
+        course: 'Finkbine Golf Course',
+        scores: [2, 4, 3, 5, 4, 3, 6, 5, 4],
+        description: 'Played 9 at fink',
+    },
+    {
+        id: 2,
+        username: 'owenlarson4',
+        course: 'Finkbine Golf Course',
+        scores: [2, 4, 3, 5, 4, 3, 6, 5, 4],
+        description: 'Played 9 at fink',
+    },
+    {
+        id: 2,
+        username: 'owenlarson5',
         course: 'Finkbine Golf Course',
         scores: [2, 4, 3, 5, 4, 3, 6, 5, 4],
         description: 'Played 9 at fink',
