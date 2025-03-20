75ee0dea1c5b6ebccda0ca64abdbc43cacfc3c56
owenslar <owenslar@byu.edu>
2025-03-20 10:52:34 -0600

    create page started

--- pages/create.vue
+++ pages/create.vue
@@ -1,3 +1,31 @@
 <template>
     Create page
+    <form @submit.prevent="handleSubmit">
+        <label for="name">Name:</label>
+        <input type="text" id="name" v-model="formData.name"><br>
+
+        <label for="course">Course:</label>
+        <input type="text" id="course" v-model="formData.course"><br>
+
+        <button type="submit">Submit</button>
+    </form>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                formData: {
+                    name: '',
+                    course: '',
+                }
+            }
+        },
+        methods: {
+            handleSubmit() {
+                const jsonData = JSON.stringify(this.formData);
+                console.log(jsonData);
+            }
+        }
+    }
+</script>
