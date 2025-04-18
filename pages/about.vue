008703ada9e4f2bcfa5d805018c7dfa4e62ade9e
owenslar <owenslar@byu.edu>
2025-04-18 11:36:29 -0600

    Github Actions Workflow test

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col">
         <div class="mt-8 mb-4 text-4xl font-bold text-center">About this site</div>
-        <p class="m-4 text-2xl text-center">This is a full stack site built by Owen Larson as part of training at BYU Broadcasting. This website was built from scratch using Nuxt/Vue and Tailwind CSS for the frontend, and Node Express for the backend using DynamoDB. Authentication for this site was implemented using JsonWebTokens with both access tokens and refresh tokens. This website was containerized with Docker and deoplyed using AWS ECS. In this site users can connect over their golf rounds. Users can view blogs that have been posted, as well as create, post, and delete their own blogs.</p>
+        <p class="m-4 text-2xl text-center">This is a full stack site built by Owen Larson as part of training at BYU Broadcasting. This website was built from scratch using Nuxt/Vue and Tailwind CSS for the frontend, and Nuxt Native for the backend using DynamoDB. Authentication for this site was implemented using JsonWebTokens with both access tokens and refresh tokens. This website was containerized with Docker and deployed using AWS ECS. AWS resources used in the building of this site include EC2, ECS, VPC, IAM, ASM, DynamoDB and Route 53. In this site users can connect over their golf rounds. Users can view blogs that have been posted, as well as create, post, and delete their own blogs.</p>
     </div>
 </template>
